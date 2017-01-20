c022d9e85cdd6b4b5f5bfbbde74a0d5c223b7f77
FondFish <844875798@qq.com>
2017-01-20 15:48:26 +0800

    Create config.h

---
+++ config.h
@@ -0,0 +1,21 @@
+#ifndef _OSS_CONFIG_H
+#define _OSS_CONFIG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "sche.h"
+
+extern T_CoreData*         M_ptCoreData;
+
+#define M_atScheTCB                         (M_ptCoreData->atScheTCB)
+#define M_atPCB                             (M_ptCoreData->ptPCB)
+
+extern  T_QueueCtl   s_atQueueCtl[MAX_QUEUE_COUNT];
+extern  T_ThreadInfo                      g_threadInfo[SCHE_TASK_NUM];
+
+extern    int    g_procNum;
+extern    T_ProcessConfig gaProcCfgTbl[];
+extern    T_TaskConfig    gaScheTaskConfig[SCHE_TASK_NUM]; 
+
+
+#endif
