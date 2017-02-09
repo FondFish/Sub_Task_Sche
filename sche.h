9ed854589d184ec2d755bafd57da317b4299c63d
FondFish <844875798@qq.com>
2017-02-09 18:20:24 +0800

    Update sche.h

--- sche.h
+++ sche.h
@@ -1,4 +1,5 @@
 
+
 #ifndef _OSS_SCHELIB_H
 #define _OSS_SCHELIB_H
 
@@ -6,43 +7,41 @@
 #include <stdio.h>
 #include <ucontext.h>
 #include <semaphore.h>
-
-#define VOID void 
-typedef void*               LPVOID;
-typedef unsigned long WORD32;
-typedef unsigned short      WORD16;
-typedef unsigned char   BYTE;
-typedef unsigned long  ULONG;
-typedef char                CHAR;
-typedef signed long         SWORD32;
-typedef unsigned long   OSS_ULONG;
-typedef BYTE    BOOLEAN;
-typedef signed long       OSS_LONG;
-typedef int       INT;
-typedef unsigned short  WORD;
+#include "type.h"
+#include "comm.h"
 
 typedef VOID (*UniBTSProcEntry_Type)(WORD16, WORD16, VOID *, VOID *);
-typedef VOID (*TaskEntryProto)(LPVOID); 
+typedef VOID (*TaskEntryProto)(LPVOID); /* ÈÎÎñÈë¿ÚÔ­ÐÍ */
 
 /*ÏûÏ¢¶ÓÁÐÏà¹Ø*/
-#define VOS_CREATE_QUEUE_FAILURE    (WORD32)0xFFFFFFFF  
+#define VOS_CREATE_QUEUE_FAILURE    (WORD32)0xFFFFFFFF  /* ´´½¨¶ÓÁÐÊ§°Ü */
 #define MAX_QUEUE_COUNT             (WORD32)10
-#define VOS_QUEUE_CTL_USED          (BYTE)0x01  
-#define VOS_QUEUE_CTL_NO_USED       (BYTE)0x10 
+#define VOS_QUEUE_CTL_USED          (BYTE)0x01  /* ¶ÓÁÐ¿ØÖÆ¿é±»Ê¹ÓÃ */
+#define VOS_QUEUE_CTL_NO_USED       (BYTE)0x10  /* ¶ÓÁÐ¿ØÖÆ¿éÎ´±»Ê¹ÓÃ */
+#define VOS_QUEUE_ERROR             (SWORD32)0xEFFFFFFF /* ±íÃ÷³ö¶Ó²Ù×÷Ê§°Ü */
+
 #define MSQNAME "/OSS_MQ%d"
 
 #define    SCHE_TASK_NUM              2
 #define    TASK_NAME_LEN               60
-#define    QUEUE_NAME_LEN              (WORD32)40   
+#define    QUEUE_NAME_LEN              (WORD32)40   /* ¶ÓÁÐÃû×Ö³¤¶È */ 
 
 /* ½ø³Ì×´Ì¬±êÊ¶ */
-#define    PROC_STATUS_READY                ((BYTE)1)  
-#define    PROC_STATUS_RUNNING              ((BYTE)2) 
-#define    PROC_STATUS_BLOCK                ((BYTE)3)  
+#define    PROC_STATUS_READY                ((BYTE)1)  /* ½ø³ÌÎ»ÓÚ¾ÍÐ÷¶ÓÁÐ */
+#define    PROC_STATUS_RUNNING              ((BYTE)2)  /* ½ø³ÌÕýÔÚÔËÐÐ */
+#define    PROC_STATUS_BLOCK                ((BYTE)3)  /* ½ø³Ì´¦ÓÚ×èÈû¶ÓÁÐ */
+/* ½ø³Ì×èÈûµÈ´ýÔ­Òò */
+#define    WAIT_MSG                         (0)  /* ÎÞµÈ´ý */
+#define    WAIT_REMOTECALL                  (1)  /* Ô¶³Ì¹ý³Ìµ÷ÓÃµÈ´ý */
+#define    WAIT_SYNCMSG                     (2)  /* Í¬²½ÏûÏ¢µÈ´ý */
+#define    WAIT_SYNCTIMER                   (3)  /* Í¬²½¶¨Ê±Æ÷µÈ´ý */
+
+#define EV_STARTUP                              (WORD16)100
 
 #define WEOF               (WORD16)(0xFFFF)
-#define OSS_SUCCESS      0
-#define OSS_ERROR_UNKNOWN     1
+
+#define SUCCESS      0
+#define ERROR_UNKNOWN     1
 
 #define  YES     1
 #define  NO      0
@@ -52,35 +51,36 @@ typedef VOID (*TaskEntryProto)(LPVOID);
 
 #define  S_StartUp     0
 
-#define TASK_NAME_LENGTH        21     
-#define PROC_NAME_LENGTH        30     
-#define INVALID_SYS_TASKID          (WORD32)0  
+#define TASK_NAME_LENGTH        21      /* ÈÎÎñÃû³¤¶È */
+#define PROC_NAME_LENGTH        30      /* ½ø³ÌÃû×Ö·û´®³¤¶È*/
+#define INVALID_SYS_TASKID          (WORD32)0   /* ÎÞÐ§µÄÏµÍ³ÈÎÎñID */
 
 #define PROC_TYPE_INDEX_NUM     ((WORD16)0xFFFF)
 
 typedef struct tagT_ProcessName
 {
-    WORD16  wProcType;               
-    CHAR    acName[PROC_NAME_LENGTH];
+    WORD16  wProcType;               /* ½ø³ÌÀàÐÍºÅ */
+    CHAR    acName[PROC_NAME_LENGTH];/* ½ø³ÌÃû */
 }T_ProcessName;
 
 typedef struct tagT_TaskConfig
 {
-    BYTE    ucIsUse;                 
-    CHAR    acName[TASK_NAME_LENGTH]; 
-    WORD16  wPriority;               
-    WORD32  dwStacksize;             
-    LPVOID  pEntry;                  
+    BYTE    ucIsUse;                  /* ÈÎÎñÊÇ·ñÊ¹ÓÃ */
+    CHAR    acName[TASK_NAME_LENGTH]; /* ÈÎÎñÃû */
+    WORD16  wPriority;                /* ÈÎÎñÓÅÏÈ¼¶ */
+    WORD32  dwStacksize;              /* ÈÎÎñÕ»´óÐ¡£¬Ö»ÄÜÅäÖÃ³ÉÒ³´óÐ¡µÄÕûÊý±¶ */
+    LPVOID  pEntry;                   /* ÈÎÎñÈë¿Úº¯Êý */
 }T_TaskConfig ;
 
+/* ½ø³ÌÅäÖÃ±íÏî½á¹¹ */
 typedef struct tagT_ProcessConfig
 {
-    BYTE    ucIsUse;               
-    WORD16  wProcType;          
-    WORD16  wTno;                 
-    LPVOID  pEntry;               
-    WORD32  dwStackSize;          
-    WORD32  dwDataRegionSize;      
+    BYTE    ucIsUse;                 /* ½ø³ÌÊÇ·ñÊ¹ÓÃ */
+    WORD16  wProcType;               /* ½ø³ÌÀàÐÍºÅ */
+    WORD16  wTno;                    /* ½ø³ÌËùÊôÓÚµÄµ÷¶ÈÈÎÎñºÅ */
+    LPVOID  pEntry;                  /* ½ø³ÌÈë¿Úº¯Êý */
+    WORD32  dwStackSize;             /* ½ø³ÌÕ»´óÐ¡£¬Ö»ÄÜÅäÖÃ³ÉÒ³´óÐ¡µÄÕûÊý±¶ */
+    WORD32  dwDataRegionSize;        /* ½ø³ÌÊý¾ÝÇø´óÐ¡£¬Ö»ÄÜÅäÖÃ³ÉÒ³´óÐ¡µÄÕûÊý±¶ */
 }T_ProcessConfig;
 
 typedef struct tagT_ScheTCB
@@ -92,45 +92,69 @@ typedef struct tagT_ScheTCB
     void (*pEntry)(WORD32);
     WORD32   dwMailBoxId;
     ULONG   dwTaskId;
-    WORD16  wBlockHead;         
-    WORD16  wReadyHead;     
-    WORD16  wRunning;       
-    WORD16  wReadyCounts;       
-    WORD16  wBlockCounts;     
-    WORD32  dwScheCounts;         
-    WORD16  wProcTypeCounts;   
-    BYTE	bEnterProc; 			
+
+    WORD16  wBlockHead;             /* ÈÎÎñÖÐ×èÈû½ø³Ì¶ÓÁÐÍ·Ë÷Òý */
+    WORD16  wReadyHead;             /* ÈÎÎñÖÐ¾ÍÐ÷½ø³Ì¶ÓÁÐÍ·Ë÷Òý */
+    WORD16  wRunning;               /* ÈÎÎñÕýÔÚÔËÐÐµÄ½ø³ÌµÄË÷Òý */
+
+    WORD16  wReadyCounts;           /* ±¾ÈÎÎñÖÐµÄready½ø³ÌÊý */
+    WORD16  wBlockCounts;           /* ±¾ÈÎÎñÖÐµÄblock½ø³ÌÊý */
+
+    WORD32  dwScheCounts;           /* ÈÎÎñ±»µ÷¶È´ÎÊý */    
+    WORD16  wProcTypeCounts;        /* ±¾ÈÎÎñÖÐµÄ½ø³ÌÀàÐÍÊý */
+
+    BYTE	bEnterProc; 			/*ÊÇ·ñ¿ªÊ¼µ÷¶È½ø³Ì*/
 }T_ScheTCB;
 
 typedef struct tagT_PCB
 {
-    BYTE        ucIsUse;          
-    BYTE        ucRunStatus;     
-    WORD16      wTno;          
-    WORD16      wPrevByReady;  
-    WORD16      wNextByReady;   
-    WORD16      wState;           
-    WORD16      wInstanceNo;      
-    WORD16      wProcType;       
+    BYTE        ucIsUse;            /* ½ø³ÌÊÇ·ñÊ¹ÓÃ */
+    BYTE        ucRunStatus;        /* Ready(Î»ÓÚ¾ÍÐ÷¶ÓÁÐ),Block(Î»ÓÚ×èÈû¶ÓÁÐ),Run*/
+    WORD16      wTno;               /* ½ø³ÌËùÔÚµÄÈÎÎñ±êÊ¶ */
+
+    /*×èÈû/ÔËÐÐÁ´±í,¸ù¾ÝÕâ¸öPCB×´Ì¬¶ø¶¨*/
+    WORD16      wPrevByStatus;   /*Ç°Ò»¸öÔËÐÐ»ò×èÈûµÄPCB*/
+    WORD16      wNextByStatus;   /*ºóÒ»¸öÔËÐÐ»ò×èÈûµÄPCB*/
+
+    WORD16      wState;             /* ½ø³ÌµÄÒµÎñ×´Ì¬ */
+    WORD16      wRetState;             /* ½ø³ÌµÄ·µ»Ø×´Ì¬ */
+    WORD16      wInstanceNo;        /*½ø³ÌÊµÀýºÅ */
+    WORD16      wProcType;          /* ½ø³ÌÀàÐÍ */
+
     VOID     (*pEntry)(void);        
-    ULONG      dwProcSP;         
-    ULONG      dwTaskSP;          
-    BYTE        *pucStack;         
-    WORD32      dwStackSize ;      
+    ULONG      dwProcSP;           /* ½ø³Ì¶ÑÕ»¶¥Ö¸Õë */
+    ULONG      dwTaskSP;           /* ÈÎÎñ¶ÑÕ»¶¥Ö¸Õë */
+    BYTE        *pucStack;          /* ½ø³Ì¶ÑÕ»µ×Ö¸Õë */
+    WORD32      dwStackSize ;       /* ¶ÑÕ»´óÐ¡ */
+    
+    BYTE        *pucDataRegion;     /* Ë½ÓÐÊý¾ÝÇøÖ¸Õë */
     WORD32      dwDataRegionSize;
-    WORD32      dwScheCounts;          
+
+    BYTE        ucBlockReason;      /* PCB×èÈûµÈ´ýÔ­Òò */
+    WORD32      dwScheCounts;           /* ¾àÉÏ´Î½ø³ÌÆµ¶ÈÍ³¼ÆÊ±µÄµ÷¶È´ÎÊý */
+    
+    T_MSG       *ptCurMsg;   /* µ±Ç°ÏûÏ¢ */
+    WORD32      dwMsgQCounts;            /* ½ø³ÌÏûÏ¢¶ÓÁÐÖÐµÄÏûÏ¢Êý */
+    /* ½ø³ÌÏûÏ¢¶ÓÁÐÖ¸Õë */
+    T_MSG       *ptPrevMsg;         /* ½ø³Ìµ±Ç°ÏûÏ¢µÄÇ°Ò»¸öÏûÏ¢Ö¸Õë */
+    T_MSG       *ptFirstMsg ;       /* ½ø³ÌÏûÏ¢¶ÓÁÐµÄµÚÒ»ÌõÏûÏ¢ */
+    T_MSG       *ptLastMsg ;        /* ½ø³ÌÏûÏ¢¶ÓÁÐµÄ×îºóÒ»ÌõÏûÏ¢ */
 }T_PCB;
 
 typedef struct tQCtlBlk
 {
-    WORD32      dwTotalCount;       
-    WORD32      dwEntrySize;       
-    WORD32      dwUsedCount;      
-    WORD32      dwQueueId;       
-    BYTE        ucUsed;            
-    CHAR        aucQueueName[QUEUE_NAME_LEN]; 
-    WORD16      wLostMsgQFull;    
-    CHAR        pad[2];           
+    WORD32      dwTotalCount;       /* ¶ÓÁÐÔªËØ×Ü¸öÊý*/
+    WORD32      dwEntrySize;        /* ¶ÓÁÐÔªËØ³ß´ç */
+
+    WORD32      dwUsedCount;        /* ÏÖÓÐµÄ¶ÓÁÐÔªËØ¸öÊý */
+    WORD32      dwQueueId;        /* VxWorks¶ÓÁÐµÄÈÎÎñID */
+
+    BYTE        ucUsed;             /* Ê¹ÓÃ±êÖ¾ */
+
+    CHAR        aucQueueName[QUEUE_NAME_LEN]; /* ¶ÓÁÐÃû×Ö */
+
+    WORD16      wLostMsgQFull;     /*¼ÇÂ¼¶ÓÁÐÂú¶ªÆúµÄÏûÏ¢Êý*/ 
+    CHAR        pad[2];            /*¶ÔÆëÌî³ä×Ö½Ú*/	
     WORD32      dwUsedMaxNum;
 }T_QueueCtl;
 
@@ -143,23 +167,28 @@ typedef  struct ThreadInfo_tag
     VOID            *arg;
     WORD32      thread_id;
     WORD32          thread_tid; 
-    INT             stacksize;      
-    WORD32          stackguardsize; 
-    OSS_ULONG         stackaddr;     
+    INT             stacksize;      /* Ïß³Ì¶ÑÕ»´óÐ¡ */
+    WORD32          stackguardsize; /* Ïß³Ì¶ÑÕ»±£»¤Ò³´óÐ¡ */
+    ULONG         stackaddr;      /* Ïß³Ì¶ÑÕ»µÍµØÖ·ÆðÊ¼´¦ */
     WORD32          tSpareInfo;
-    BYTE            *pbySigAltStack;
+    BYTE            *pbySigAltStack;/* Ïß³ÌÐÅºÅ´¦Àíº¯Êý×¨ÓÃÕ»*/
 }T_ThreadInfo;
 
 typedef struct tagT_CoreData                    
 {
-    WORD32          dwCoreDataSize; 
-    WORD16          wPCBCounts;       
-    T_PCB           *ptPCB;          
-    WORD32          dwProcNameCounts;  
-    T_ProcessName   *pProcNameTable;   
-    T_ScheTCB       atScheTCB[SCHE_TASK_NUM]; 
-    WORD32          dwSelfPCBIndex;              
-    BYTE            pIdx; 
+    WORD32          dwCoreDataSize;               /* ½ø³Ì¼¶:ºËÐÄÊý¾ÝÇøµÄ´óÐ¡ */
+
+    WORD16          wPCBCounts;                   /* µÍ³ÖÐµÄPCB×ÊÔ´ÊýÁ¿ */
+    T_PCB           *ptPCB;                       /* ÏµÍ³µÄµÚÒ»¸öPCB¿éÖ¸Õë */
+
+    WORD32          dwProcNameCounts;             /* ½ø³Ì¼¶:È«¾Ö½ø³ÌÃû¸öÊý */
+    T_ProcessName   *pProcNameTable;              /* ½ø³Ì¼¶:È«¾Ö½ø³ÌÃû±í */
+
+    T_ScheTCB       atScheTCB[SCHE_TASK_NUM];     /* ½ø³Ì¼¶:µ÷¶ÈÈÎÎñTCBÊý×é */
+
+    WORD32          dwSelfPCBIndex;               /* ½ø³Ì¼¶:µ±Ç°ÔËÐÐµÄ½ø³ÌµÄË÷Òý */
+    
+    BYTE            pIdx;  /* µ±Ç°½ø³ÌÔÚ½ø³ÌË÷Òý±íÖÐµÄÎ»ÖÃ */
 }T_CoreData;
 
 
@@ -169,3 +198,4 @@ extern void NormalReturnToTask(T_PCB *ptPCB);
 
 
 #endif
+
