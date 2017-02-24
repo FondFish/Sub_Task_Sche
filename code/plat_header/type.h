7cc984f908b9c287b15ed8aba79cd0bc0db5435e
FondFish <844875798@qq.com>
2017-02-24 21:58:44 +0800

    Create type.h

---
+++ code/plat_header/type.h
@@ -0,0 +1,18 @@
+
+#ifndef _TYPELIB_H
+#define _TYPELIB_H
+
+#define VOID void 
+typedef void*               LPVOID;
+typedef unsigned long WORD32;
+typedef unsigned short      WORD16;
+typedef unsigned char   BYTE;
+typedef unsigned long  ULONG;
+typedef char                CHAR;
+typedef signed long         SWORD32;
+typedef BYTE    BOOLEAN;
+typedef signed long       LONG;
+typedef int       INT;
+typedef unsigned short  WORD;
+
+#endif
